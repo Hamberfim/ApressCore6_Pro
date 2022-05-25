9273b072ae4c8665b78c7abcfd4b7aac3e6977ef
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-25 07:01:31 -0500

    chapt2, First Project

---
+++ Core6_Apress/_02_FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Index Method return a ViewResult
+        public ViewResult Index() 
+        {
+            // provide data to the view by passing arguments
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";  // tenary operator
+            return View("MyView", viewModel);
+        }
+    }
+}
