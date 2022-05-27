f91e5c196560fa4258dffd71743cd5de67069e90
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-27 15:14:17 -0500

    started chapt 3

---
+++ Core6_Apress/_03_PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using _03_PartyInvites.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _03_PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Core6_Apress/_03_PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace _03_PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        // '?' - nullable 
+        public string? Name  { get; set; }
+
+        public string? Email  { get; set; }
+
+        public string? Phone  { get; set; }
+
+        public bool? WillAttend { get; set; }
+
+    }
+}
