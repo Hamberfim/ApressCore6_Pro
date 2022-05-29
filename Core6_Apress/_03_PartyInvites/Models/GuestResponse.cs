c36708839dc14f1eca07674573227be107525402
Anthony Hamlin <adhamlingithub@gmail.com>
2022-05-29 13:36:52 -0500

    rsvp form validation, css, bootstrap style

--- Core6_Apress/_03_PartyInvites/Models/GuestResponse.cs
+++ Core6_Apress/_03_PartyInvites/Models/GuestResponse.cs
@@ -9,9 +9,11 @@ namespace _03_PartyInvites.Models
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Email address is required")]
+        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please enter your phone number")]
+        [DataType(DataType.PhoneNumber)]
         public string? Phone { get; set; }
 
         [Required(ErrorMessage = "Please specify whether you'll attend")]
