397f14ee0bf7c676ac068d20821bf8fe6f44b545
Anthony Hamlin <adhamlingithub@gmail.com>
2022-06-04 17:27:24 -0500

    stopped pg 69 - chapt 11

--- CsharpQuickRef/_11_inheritance/Program.cs
+++ CsharpQuickRef/_11_inheritance/Program.cs
@@ -54,7 +54,7 @@
             personInfoUpcast.lName = "Hill";
             personInfoUpcast.age = 44;
             personInfoUpcast.gender = "male";
-            
+
             // upcast 
             Person personUpcast = personInfoUpcast;
             // Console.WriteLine(personUpcast.personIdentity());  // ERROR - only Person' members can be access
@@ -71,7 +71,53 @@
             // unboxing via cast
             price = (double)priceObj;
 
-            // is | as keywords
+            Console.WriteLine();  // space in output
+
+            // 'is' | 'as' keywords - used to avoid exception when casting
+            var n = 12;
+            var d = 2.2333;
+            if (n is double)
+            {
+                int p = (int)d;
+                Console.WriteLine(p);
+            }
+
+            int ten = 10;
+            object tenObj = ten;
+            string tenStr = tenObj as string;  //only works with ref types - invalid cast returns a null
+            Console.WriteLine(tenStr is null);
+
+            Console.WriteLine();  // space in output
+
+            // 'is'  - pattern matching
+            object obj = "Some Text";
+            if (!(obj is string text))
+            {
+                return;
+            }
+            Console.WriteLine(text);
+
+            Console.WriteLine();  // space in output
+
+            void test(object j)
+            {
+                if (j is 5)
+                {
+                    Console.WriteLine(5);
+                }
+                else if (j is int i)
+                {
+                    Console.WriteLine("int:" + i);
+                }
+                else if (j is null)
+                {
+                    Console.WriteLine("null");
+                }
+            }
+
+            test(5);
+            test(1);
+            test(null);
 
         }
     }
