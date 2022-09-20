eb42a1a41b454b78b3e8e5f8e8d7889434f42338
Hamberfim <adhamlingithub@gmail.com>
2022-09-20 14:54:12 -0500

    finished chapt3

---
+++ Core6_Apress/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please Enter your name.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your email address.")]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your phone number.")]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify if you will attend or not.")]
+        public bool? WillAttend { get; set; }
+    }
+}
