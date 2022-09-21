a7ec7109550d26c175e9384f39cc9cabf9f59caa
Hamberfim <adhamlingithub@gmail.com>
2022-09-21 14:00:24 -0500

    some lang features

---
+++ Core6_Apress/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿//using LanguageFeatures.Models;  using in now in the GlobalUsing.cs
+//using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+
+            //return View(new string[] { "C#", "Language", "Features" });
+            // 1. checking for null values
+            Product?[] products = Product.GetProducts();
+            // return View(new string[] { products[0].Name });
+
+            // 2. guarding against null values
+            //Product? p = products[0];
+            //string val;
+            //if (p != null)
+            //{
+            //    val = p.Name;
+            //}
+            //else
+            //{
+            //    val = "No Value";
+            //}
+            //return View(new string[] { val });
+
+            // 3. null conditional operator
+            //string? val = products[0]?.Name;
+            //if (val != null)
+            //{
+            //    return View(new string[] { val });
+            //}
+            //return View(new string[] { "No Value" });
+
+            // 4. null coalescing operator
+            // return View(new string[] { products[0]?.Name ?? "No value" });
+
+            // 5. overriding Null Analysis
+            // return View(new string[] { products[0]!.Name }); // null forgiving operator - guaranteed not to be null by programmer
+
+            // string interpolation
+            // return View(new string[] { $"{products[0]?.Name} for {products[0]?.Price:c2}" });
+
+            string[] productListings = new string[products.Length];
+            for (int i = 0; i < products.Length; i++)
+            {
+                productListings[i] = ($"{products[i]?.Name} for {products[i]?.Price:c2}");
+            }
+            return View(productListings);
+        }
+    }
+}
---
+++ Core6_Apress/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public static Product?[] GetProducts()
+        {
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275m
+            };
+
+            Product lifeJacket = new Product
+            {
+                Name = "Life Jacket",
+                Price = 48.95m
+            };
+
+            Product paddle = new Product
+            {
+                Name = "Paddle",
+                Price = 56.95m
+            };
+
+            return new Product?[] { kayak, lifeJacket, paddle };
+        }
+    }
+}
